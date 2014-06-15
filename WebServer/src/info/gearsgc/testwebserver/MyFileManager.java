50408dedcab8de0f951a9a5d573ebcf69e918205
Greg Zhou <bottlezz2002@gmail.com>
2014-06-15 14:55:30 -0700

    replace webserver folder

---
+++ WebServer/src/info/gearsgc/testwebserver/MyFileManager.java
@@ -0,0 +1,5 @@
+package info.gearsgc.testwebserver;
+
+public class MyFileManager {
+	
+}
---
+++ WebServer/src/info/gearsgc/webserver/GcFileManager.java
@@ -0,0 +1,10 @@
+package info.gearsgc.webserver;
+
+public interface GcFileManager {
+	public void close();
+	
+	public java.io.InputStream open(java.lang.String fileName) throws java.io.IOException ;
+	
+	public java.io.InputStream open(java.lang.String fileName, int accessMode) throws java.io.IOException;
+	
+}
