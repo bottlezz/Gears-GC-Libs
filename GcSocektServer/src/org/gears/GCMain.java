5283d92833d992f08df1c1dfb45aa3479d0a74e7
Gregory Zhou <bottlezz2002@gmail.com>
2015-02-04 15:37:16 -0800

    added exception
    Catch exception and exit in main function.

--- GcSocektServer/src/org/gears/GCMain.java
+++ GcSocektServer/src/org/gears/GCMain.java
@@ -13,7 +13,10 @@ public class GCMain
 		}
 		catch (Exception e)
 		{
-			
+			e.printStackTrace(); //if you want it.
+		    //You could always just System.out.println("Exception occurred.");
+		    //Though the above is rather unspecific.
+		    System.exit(1);
 		}
 	}
 }
